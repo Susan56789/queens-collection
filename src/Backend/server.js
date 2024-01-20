8c5770feba67f63a3196e797bfb6e6baa2723868
Susan56789 <devnimoh@gmail.com>
2024-01-20 12:52:41 +0300

    Backend setup

--- src/Backend/server.js
+++ src/Backend/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const { Pool } = require('pg');
 const bodyParser = require('body-parser');
-const apiRoutes = require('./routes/apiRoutes');
+const apiRoutes = require('./apiRoutes');
 const app = express();
 const port = process.env.PORT || 3000;
 
