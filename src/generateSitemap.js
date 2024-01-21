5e4f4bf2110ac7aa64727e8ebc22c014f5701aa6
susan56789 <devnimoh@gmail.com>
2024-01-21 12:52:19 +0300

    generate sitemap

--- src/generateSitemap.js
+++ src/generateSitemap.js
@@ -1,5 +1,7 @@
 const SitemapGenerator = require('sitemap-generator');
 
+
+
 // Set the URL of your Vue.js app
 const targetUrl = 'https://queenscollection.vercel.app';
 
@@ -8,18 +10,20 @@ const outputPath = './public/sitemap.xml';
 
 // Initialize the generator
 const generator = SitemapGenerator(targetUrl, {
-  stripQuerystring: false, // Keep query parameters in URLs
-  filepath: outputPath,    // Output path for the sitemap
+    stripQuerystring: false, // Keep query parameters in URLs
+    filepath: outputPath,    // Output path for the sitemap
 });
 
 // Register event listeners
 generator.on('done', () => {
-  console.log('Sitemap generated successfully.');
+    console.log('Sitemap generated successfully.');
 });
 
 generator.on('error', (error) => {
-  console.error('Error generating sitemap:', error);
+    console.error('Error generating sitemap:', error);
 });
 
 // Start the generation process
 generator.start();
+
+// node src/generateSitemap.js
