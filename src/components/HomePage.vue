7ea2075725776174466c26c3b510145b29214fe4
Susan56789 <devnimoh@gmail.com>
2024-01-22 16:59:59 +0300

    fetch products

--- src/components/HomePage.vue
+++ src/components/HomePage.vue
@@ -65,9 +65,10 @@
             <div class="mt-16">
                 <h3 class="text-gray-600 text-2xl font-medium">New Arrivals</h3>
                 <div class="grid gap-6 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mt-6">
-                    <div class="w-full max-w-sm mx-auto rounded-md shadow-md overflow-hidden">
+                    <div v-for="product  in allProducts.slice(0, 8)" :key="product.product_id"
+                        class="w-full max-w-sm mx-auto rounded-md shadow-md overflow-hidden">
                         <div class="flex items-end justify-end h-56 w-full bg-cover"
-                            style="background-image: url('https://images.unsplash.com/photo-1563170351-be82bc888aa4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=376&q=80')">
+                            :style="{ backgroundImage: 'url(' + product.image_path + ')' }">
                             <button
                                 class="p-2 rounded-full bg-red-600 text-white mx-5 -mb-4 hover:bg-red-500 focus:outline-none focus:bg-red-500">
                                 <svg class="h-5 w-5" fill="none" stroke-linecap="round" stroke-linejoin="round"
@@ -79,72 +80,25 @@
                             </button>
                         </div>
                         <div class="px-5 py-3">
-                            <h3 class="text-gray-700 uppercase">Chanel</h3>
-                            <span class="text-gray-500 mt-2">$12</span>
+                            <h3 class="text-gray-700 uppercase">{{ product.product_name }}</h3>
+                            <span class="text-gray-500 mt-2">{{ formatNumber(product.price) }}</span>
                         </div>
                     </div>
-                    <div class="w-full max-w-sm mx-auto rounded-md shadow-md overflow-hidden">
-                        <div class="flex items-end justify-end h-56 w-full bg-cover"
-                            style="background-image: url('https://images.unsplash.com/photo-1544441893-675973e31985?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80')">
-                            <button
-                                class="p-2 rounded-full bg-red-600 text-white mx-5 -mb-4 hover:bg-red-500 focus:outline-none focus:bg-red-500">
-                                <svg class="h-5 w-5" fill="none" stroke-linecap="round" stroke-linejoin="round"
-                                    stroke-width="2" viewBox="0 0 24 24" stroke="currentColor">
-                                    <path
-                                        d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z">
-                                    </path>
-                                </svg>
-                            </button>
-                        </div>
-                        <div class="px-5 py-3">
-                            <h3 class="text-gray-700 uppercase">Man Mix</h3>
-                            <span class="text-gray-500 mt-2">$12</span>
-                        </div>
-                    </div>
-                    <div class="w-full max-w-sm mx-auto rounded-md shadow-md overflow-hidden">
-                        <div class="flex items-end justify-end h-56 w-full bg-cover"
-                            style="background-image: url('https://images.unsplash.com/photo-1532667449560-72a95c8d381b?ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80')">
-                            <button
-                                class="p-2 rounded-full bg-red-600 text-white mx-5 -mb-4 hover:bg-red-500 focus:outline-none focus:bg-red-500">
-                                <svg class="h-5 w-5" fill="none" stroke-linecap="round" stroke-linejoin="round"
-                                    stroke-width="2" viewBox="0 0 24 24" stroke="currentColor">
-                                    <path
-                                        d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z">
-                                    </path>
-                                </svg>
-                            </button>
-                        </div>
-                        <div class="px-5 py-3">
-                            <h3 class="text-gray-700 uppercase">Classic watch</h3>
-                            <span class="text-gray-500 mt-2">$12</span>
-                        </div>
-                    </div>
-                    <div class="w-full max-w-sm mx-auto rounded-md shadow-md overflow-hidden">
-                        <div class="flex items-end justify-end h-56 w-full bg-cover"
-                            style="background-image: url('https://images.unsplash.com/photo-1590664863685-a99ef05e9f61?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=345&q=80')">
-                            <button
-                                class="p-2 rounded-full bg-red-600 text-white mx-5 -mb-4 hover:bg-red-500 focus:outline-none focus:bg-red-500">
-                                <svg class="h-5 w-5" fill="none" stroke-linecap="round" stroke-linejoin="round"
-                                    stroke-width="2" viewBox="0 0 24 24" stroke="currentColor">
-                                    <path
-                                        d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z">
-                                    </path>
-                                </svg>
-                            </button>
-                        </div>
-                        <div class="px-5 py-3">
-                            <h3 class="text-gray-700 uppercase">woman mix</h3>
-                            <span class="text-gray-500 mt-2">$12</span>
-                        </div>
-                    </div>
+
+
                 </div>
             </div>
             <div class="mt-16">
                 <h3 class="text-gray-600 text-2xl font-medium">Today's Offer</h3>
                 <div class="grid gap-6 grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 mt-6">
-                    <div class="w-full max-w-sm mx-auto rounded-md shadow-md overflow-hidden">
+                    <div v-for="product  in productsOnSale.slice(0, 8)" :key="product.product_id"
+                        class="w-full max-w-sm mx-auto rounded-md shadow-md overflow-hidden relative">
                         <div class="flex items-end justify-end h-56 w-full bg-cover"
-                            style="background-image: url('https://images.unsplash.com/photo-1563170351-be82bc888aa4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=376&q=80')">
+                            :style="{ backgroundImage: 'url(' + product.image_path + ')' }">
+                            <button v-if="product.amountSaved > 0" class="save-button absolute top-0 right-0 bg-green-500 
+                text-white p-2 duration-100 hover:bg-green-700 rounded-full">
+                                SAVE<br />{{ formatNumber(product.amountSaved) }}
+                            </button>
                             <button
                                 class="p-2 rounded-full bg-red-600 text-white mx-5 -mb-4 hover:bg-red-500 focus:outline-none focus:bg-red-500">
                                 <svg class="h-5 w-5" fill="none" stroke-linecap="round" stroke-linejoin="round"
@@ -156,62 +110,8 @@
                             </button>
                         </div>
                         <div class="px-5 py-3">
-                            <h3 class="text-gray-700 uppercase">Chanel</h3>
-                            <span class="text-gray-500 mt-2">$12</span>
-                        </div>
-                    </div>
-                    <div class="w-full max-w-sm mx-auto rounded-md shadow-md overflow-hidden">
-                        <div class="flex items-end justify-end h-56 w-full bg-cover"
-                            style="background-image: url('https://images.unsplash.com/photo-1544441893-675973e31985?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1500&q=80')">
-                            <button
-                                class="p-2 rounded-full bg-red-600 text-white mx-5 -mb-4 hover:bg-red-500 focus:outline-none focus:bg-red-500">
-                                <svg class="h-5 w-5" fill="none" stroke-linecap="round" stroke-linejoin="round"
-                                    stroke-width="2" viewBox="0 0 24 24" stroke="currentColor">
-                                    <path
-                                        d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z">
-                                    </path>
-                                </svg>
-                            </button>
-                        </div>
-                        <div class="px-5 py-3">
-                            <h3 class="text-gray-700 uppercase">Man Mix</h3>
-                            <span class="text-gray-500 mt-2">$12</span>
-                        </div>
-                    </div>
-                    <div class="w-full max-w-sm mx-auto rounded-md shadow-md overflow-hidden">
-                        <div class="flex items-end justify-end h-56 w-full bg-cover"
-                            style="background-image: url('https://images.unsplash.com/photo-1532667449560-72a95c8d381b?ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80')">
-                            <button
-                                class="p-2 rounded-full bg-red-600 text-white mx-5 -mb-4 hover:bg-red-500 focus:outline-none focus:bg-red-500">
-                                <svg class="h-5 w-5" fill="none" stroke-linecap="round" stroke-linejoin="round"
-                                    stroke-width="2" viewBox="0 0 24 24" stroke="currentColor">
-                                    <path
-                                        d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z">
-                                    </path>
-                                </svg>
-                            </button>
-                        </div>
-                        <div class="px-5 py-3">
-                            <h3 class="text-gray-700 uppercase">Classic watch</h3>
-                            <span class="text-gray-500 mt-2">$12</span>
-                        </div>
-                    </div>
-                    <div class="w-full max-w-sm mx-auto rounded-md shadow-md overflow-hidden">
-                        <div class="flex items-end justify-end h-56 w-full bg-cover"
-                            style="background-image: url('https://images.unsplash.com/photo-1590664863685-a99ef05e9f61?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=345&q=80')">
-                            <button
-                                class="p-2 rounded-full bg-red-600 text-white mx-5 -mb-4 hover:bg-red-500 focus:outline-none focus:bg-red-500">
-                                <svg class="h-5 w-5" fill="none" stroke-linecap="round" stroke-linejoin="round"
-                                    stroke-width="2" viewBox="0 0 24 24" stroke="currentColor">
-                                    <path
-                                        d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z">
-                                    </path>
-                                </svg>
-                            </button>
-                        </div>
-                        <div class="px-5 py-3">
-                            <h3 class="text-gray-700 uppercase">woman mix</h3>
-                            <span class="text-gray-500 mt-2">$12</span>
+                            <h3 class="text-gray-700 uppercase">{{ product.product_name }}</h3>
+                            <span class="text-gray-500 mt-2">KES. {{ formatNumber(product.sale_price) }}</span>
                         </div>
                     </div>
                 </div>
@@ -221,7 +121,70 @@
 </template>
 
 <script>
+import axios from 'axios';
+
 export default {
-    name: 'HomePage'
-}
+    name: 'HomePage',
+    data() {
+        return {
+            products: [],
+            wishlist: [],
+        };
+    },
+    methods: {
+        async fetchProducts() {
+            try {
+                const response = await axios.get('http://localhost:3000/api/products');
+                this.products = response.data || [];
+                this.calculateAmountSavedForEachProduct();
+            } catch (error) {
+                console.error('Error fetching data:', error);
+            }
+        },
+
+        calculateAmountSavedForEachProduct() {
+            this.products.forEach((product) => {
+                const numericPrice = Number(product.price);
+                const numericSalePrice = Number(product.sale_price);
+
+                if (!isNaN(numericPrice) && !isNaN(numericSalePrice)) {
+                    product.amountSaved = numericPrice - numericSalePrice;
+                } else {
+                    console.error('Invalid Price or Discounted Price for a product:', product);
+                }
+            });
+        },
+
+        formatNumber(value) {
+            return value.toLocaleString();
+        },
+    },
+    computed: {
+        productsOnSale() {
+            return this.products.filter((product) => product.price > product.sale_price && product.sale_price > 0);
+        },
+        allProducts() {
+            return this.products.filter((product) => product)
+        }
+
+    },
+    mounted() {
+        this.fetchProducts();
+    },
+};
 </script>
+
+
+<style scoped>
+.save-button {
+
+    background-color: #eb2222;
+
+    color: #fff;
+
+    padding: 0.5rem;
+
+    border-radius: 80%;
+
+}
+</style>
---
+++ src/components/ShopPage/OnSale.vue
@@ -0,0 +1,232 @@
+<template>
+    <div class="bg-yellow-50 overflow-x-auto">
+        <div class="pt-32">
+            <h1 class="text-center text-2xl font-bold text-gray-800">Today's Offer</h1>
+        </div>
+        <div class="container mx-auto flex max-w-6xl flex-wrap items-center justify-between">
+            <article class="rounded-xl bg-white p-3 bg-yellow-50">
+
+                <div class="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-2 lg:grid-cols-3 gap-4">
+                    <div v-for="product in productsOnSale" :key="product.id"
+                        class="p-2 border rounded shadow-lg hover:shadow-xl hover:transform hover:scale-105 mb-4">
+                        <div class="image-container relative flex items-end overflow-hidden rounded-xl">
+                            <button v-if="product.amountSaved > 0"
+                                class="save-button sm:absolute sm:top-0 sm:right-0 bg-green-500 text-white p-2 duration-100 hover:bg-green-700 rounded-full">
+                                SAVE<br />{{ formatNumber(product.amountSaved) }}
+                            </button>
+                            <router-link :to="'/product/' + product.ProductName">
+                                <img :src="product.ImagePath" :alt="product.ProductName"
+                                    class="mx-auto w-full h-full object-cover mb-2" />
+                            </router-link>
+                            <div
+                                class="absolute bottom-2 left-2 inline-flex items-center rounded-lg bg-white p-1 shadow-md">
+                                <i class="fas fa-star text-yellow-400"></i>
+                                <span class="ml-1 text-sm text-gray-700">{{ product.Rating }}</span>
+                            </div>
+                        </div>
+                        <h2 class="text-slate-700 text-xs">{{ product.ProductName }}</h2>
+
+                        <div class="mt-2 flex items-end justify-between">
+                            <router-link :to="'/product/' + product.ProductName">
+                                <p class="text-green-500 font-semibold text-xxs">KES. {{
+                                                                    formatNumber(product.DiscountedPrice) }}</p>
+                            </router-link>
+                            <button @click="handleCartAction(product)" :class="cartButtonClass">
+                                <i :class="cartButtonIcon"></i> {{ cartButtonText }}
+                            </button>
+
+                            <button @click="addToWishlistButton(product)"
+                                class="bg-green-500 text-white px-2 py-1.5 rounded-lg duration-100 hover:bg-green-700">
+                                <i class="fas fa-heart"></i>
+                            </button>
+
+                        </div>
+
+                    </div>
+                </div>
+
+            </article>
+        </div>
+    </div>
+</template>
+    
+    
+    
+<script>
+  import axios from 'axios';
+  
+  export default {
+    data() {
+      return {
+        products: [],
+        cart: this.getCartFromStorage() || [],
+        wishlist: [],
+      };
+    },
+    mounted() {
+      this.fetchProducts();
+    },
+    methods: {
+      async fetchProducts() {
+        try {
+          const response = await axios.get('http://localhost:3000/api/products');
+          this.products = response.data || [];
+          this.calculateAmountSavedForEachProduct();
+  
+        } catch (error) {
+          console.error('Error fetching data:', error);
+        }
+      }, calculateAmountSavedForEachProduct() {
+        this.products.forEach((product) => {
+          if (product && typeof product.Price === 'number' && typeof product.DiscountedPrice === 'number') {
+            product.amountSaved = product.Price - product.DiscountedPrice;
+          } else {
+            console.error('Invalid Price or DiscountedPrice for a product:', product);
+          }
+        });
+      }, formatNumber(value) {
+  
+        return value.toLocaleString();
+      },
+  
+      addToWishlistButton(product) {
+        this.addToWishlist(product);
+      },
+      addToCartButton(product) {
+        this.addToCart(product);
+      },
+      removeFromCartButton() {
+        this.removeFromCart(this.product);
+      },
+      handleCartAction(product) {
+        if (this.isProductInCart(product)) {
+          this.removeFromCart(product);
+        } else {
+          this.addToCart(product);
+        }
+      },
+      addToWishlist(product) {
+  
+        if (product && product.ProductName && product.Price && product.ProductID) {
+  
+          if (this.isProductInWishlist(product)) {
+  
+            alert('Product already in wishlist');
+          } else {
+  
+            this.wishlist.push(product);
+  
+            axios.post('http://localhost:3000/api/addToWishlist', product)
+              .then(response => {
+                console.log(response.data);
+  
+                alert('Successfully added to wishlist');
+              })
+              .catch(error => {
+                console.error(error);
+              });
+          }
+        } else {
+          console.error('Invalid product data for wishlist:', product);
+        }
+      },
+  
+      isProductInWishlist(product) {
+  
+        return this.wishlist.some(item => item.ProductID === product.ProductID);
+      },
+  
+      addToCart(product) {
+  
+        if (!product || !product.ProductName || !product.Price) {
+          console.error('Invalid product data for cart:', product);
+          return;
+        }
+  
+  
+        if (this.isProductInCart(product)) {
+  
+          alert('Product already in cart');
+          return;
+        }
+  
+  
+        this.cart.push(product);
+        this.saveCartToStorage();
+  
+  
+        axios.post('http://localhost:3000/addToCart', product)
+          .then(response => {
+            console.log(response.data);
+  
+            alert('Successfully added to cart');
+          })
+          .catch(error => {
+            console.error('Error adding to cart:', error);
+  
+          });
+      },
+  
+      removeFromCart(product) {
+  
+        this.cart = this.cart.filter(item => item.ProductID !== product.ProductID);
+        this.saveCartToStorage();
+  
+      },
+  
+      saveCartToStorage() {
+        localStorage.setItem('cart', JSON.stringify(this.cart));
+      },
+  
+      getCartFromStorage() {
+        const storedCart = localStorage.getItem('cart');
+        return storedCart ? JSON.parse(storedCart) : [];
+      },
+  
+      isProductInCart(product) {
+  
+        if (product && product.ProductID) {
+  
+          if (this.cart) {
+  
+            return this.cart.some(item => item.ProductID === product.ProductID);
+          } else {
+            console.error('Cart is undefined');
+            return false;
+          }
+        } else {
+          console.error('Invalid product data for cart:', product);
+          return false;
+        }
+      },
+      emptyCart() {
+  
+        localStorage.removeItem('cart');
+        this.cart = [];
+      },
+  
+  
+  
+    },
+    computed: {
+      productsOnSale() {
+        return this.products.filter((product) => product.Price > product.DiscountedPrice && product.DiscountedPrice > 0);
+      },
+      cartButtonClass() {
+        return [
+          'flex items-center space-x-1.5 rounded-lg px-4 py-1.5 text-white duration-100 hover:bg-indigo-600',
+          { 'bg-indigo-500': !this.isProductInCart(this.product), 'bg-red-500': this.isProductInCart(this.product) },
+        ];
+      },
+  
+      cartButtonIcon() {
+        return this.isProductInCart(this.product) ? 'fas fa-cart-arrow-down' : 'fas fa-cart-plus';
+      },
+  
+      cartButtonText() {
+        return this.isProductInCart(this.product) ? '' : '';
+      },
+    },
+  };
+  </script>
+  
